177228b435d24b8f98c78436a538826f7fc0a7f1
Waleed-052 <108350662+Waleed-052@users.noreply.github.com>
2022-07-06 23:12:32 +0500

    Add files via upload
    some images are not uploaded due to some reason I don't know Sir but I have complete in my Laptop

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Proj.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Men()
+        {
+            return View();
+        }
+        public IActionResult Women()
+        {
+            return View();
+        }
+        public IActionResult information()
+        {
+            return View();
+        }
+        public IActionResult Helpcenter()
+        {
+            return View();
+        }
+        public IActionResult Eventz()
+        {
+            return View();
+        }
+    }
+}
